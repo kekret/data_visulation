0c44cfa0b4de0c9dd6f7a88e4761141edd73b867
kekret <141241981+kekret@users.noreply.github.com>
2023-08-03 10:46:30 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -15,6 +15,7 @@ st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
+
 y_axis = st.selectbox('select element', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
@@ -27,7 +28,7 @@ p = figure(
     x_axis_label= 'x',
     y_axis_label= 'y')
 
-p.line(el_list, el_list, legend_label='Trend', line_width=2)
+p.line(x, y, legend_label='Trend', line_width=2)
 
 st.bokeh_chart(p, use_container_width=True)
 
