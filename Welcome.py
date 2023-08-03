b3d22fe235013111b3c0752972d11de42223a745
kekret <141241981+kekret@users.noreply.github.com>
2023-08-03 12:26:58 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -19,8 +19,8 @@ y_axis = st.selectbox('select element 2', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
 
-x = df['Mg']/10000
-y = df['Si']/10000
+x = df[27:80]/10000
+y = df[27:80]/10000
 
 p = figure(
     title='Element Plot',
