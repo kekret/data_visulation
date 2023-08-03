9ecba56047387b4c94a55b4478b5a2679d0158b7
kekret <141241981+kekret@users.noreply.github.com>
2023-08-03 10:37:07 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -13,20 +13,20 @@ st.write ('Hello Word')
 df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
+el_list = df.columns.tolist()[27:80]
+x_axis = st.selectbox('select element', el_list)
+
+st.multiselect('select location', file_name_list, file_name_list[0])
+
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
 p = figure(
     title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+    x_axis_label='el_list',
+    y_axis_label='el_list')
 
 p.line(x, y, legend_label='Trend', line_width=2)
 
 st.bokeh_chart(p, use_container_width=True)
-
-el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select element', el_list)
-
-st.multiselect('select location', file_name_list, file_name_list[0])
 
