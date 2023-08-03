6d330873571c6f95c059b9d93660ddb0643c2e56
kekret <141241981+kekret@users.noreply.github.com>
2023-08-03 12:07:34 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -15,6 +15,7 @@ st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
+y_axis = st.selectbox('select element 2', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
 
